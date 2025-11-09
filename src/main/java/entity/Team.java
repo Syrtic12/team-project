43a69bae137d77a98f0a162deae28b6e3fc8079b
Sushaan Patel <sushaanpatel@gmail.com>
2025-11-09 01:22:57 -0500

    added basic code for entities

--- src/main/java/entity/Team.java
+++ src/main/java/entity/Team.java
@@ -1,4 +1,23 @@
 package entity;
+import java.util.List;
+import java.util.ArrayList;
 
+//essentially tasklist for a team
 public class Team {
+    private List<String> users;
+    private TeamLeader leader;
+    private List<Task> tasks;
+
+    public Team(TeamLeader leader){
+        this.leader = leader;
+        this.users = new ArrayList<>();
+        this.tasks = new ArrayList<>();
+    }
+
+    public List<Task> GetTasks(){
+        return this.tasks;
+    }
+
+    public void addTask(){}
+    public void removeTask(){}
 }
--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,4 +1,33 @@
 package entity;
 
+import com.google.gson.JsonObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    private String name;
+    private String email;
+    private String role;
+    private List<Team> teams;
+    private String password;
+
+    public User(String name, String email, String role, String password) {
+        this.name = name;
+        this.email = email;
+        this.role = role;
+        this.password = password;
+        this.teams = new ArrayList<>();
+    }
+
+    public void addTeam(Team team) {
+        this.teams.add(team);
+    }
+
+    public void removeTeam(Team team) {
+        this.teams.remove(team);
+    }
+
+    public JsonObject toJson() {return new JsonObject();}
+
 }
