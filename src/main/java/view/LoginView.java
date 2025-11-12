b362d4971728a164ee0a14d75b4dced9e787ee9d
Ilyas <ilyas.osmani@mail.utoronto.ca>
2025-11-12 14:22:39 -0500

    LoginView method structure added

--- src/main/java/view/LoginView.java
+++ src/main/java/view/LoginView.java
@@ -1,4 +1,34 @@
 package view;
 
-public class LoginView {
+import interface_adapter.login.LoginController;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class LoginView extends JPanel implements ActionListener, PropertyChangeListener {
+    public final String viewName = "log in";
+
+    public LoginView(){
+
+    }
+
+    public void actionPerformed(ActionEvent e){
+
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt){
+
+    }
+
+    public String getViewName(){
+        return viewName;
+    }
+
+    public void setLoginController(LoginController loginController){
+
+    }
 }
