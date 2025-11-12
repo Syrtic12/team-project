2399b013dd639cc416135c01b0b6c8fb53655be0
Ilyas <ilyas.osmani@mail.utoronto.ca>
2025-11-12 13:50:26 -0500

    viewmanager constructor and ovveride method template

--- src/main/java/view/ViewManager.java
+++ src/main/java/view/ViewManager.java
@@ -1,4 +1,18 @@
 package view;
 
-public class ViewManager {
+import java.awt.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class ViewManager implements PropertyChangeListener {
+    private final CardLayout cardLayout;
+
+    public ViewManager(CardLayout cardLayout) {
+        this.cardLayout = cardLayout;
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt){
+        
+    }
 }
