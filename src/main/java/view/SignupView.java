f272153d16b9d7df97e4fdf386b9c6c3c9ce0efe
Ilyas <ilyas.osmani@mail.utoronto.ca>
2025-11-14 14:08:39 -0500

    added empty methods for SignupView

--- src/main/java/view/SignupView.java
+++ src/main/java/view/SignupView.java
@@ -1,4 +1,51 @@
 package view;
 
-public class SignupView {
+import interface_adapter.signup.SignupController;
+import interface_adapter.signup.SignupViewModel;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class SignupView extends JPanel implements ActionListener, PropertyChangeListener {
+    private final SignupViewModel signupViewModel;
+    private final String viewName = "sign up";
+
+    public SignupView(SignupViewModel signupViewModel) {
+        this.signupViewModel = signupViewModel;
+
+    }
+
+
+    private void addUsernameListener(){
+
+    }
+
+    private void addPasswordListener(){
+
+    }
+
+    private void addRepeatPasswordListener(){
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent evt){
+
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt){
+
+    }
+
+    public String getViewName() {
+        return viewName;
+    }
+
+    public void setSignupController(SignupController controller){
+
+    }
+
 }
