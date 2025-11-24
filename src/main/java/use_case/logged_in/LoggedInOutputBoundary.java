65d1b167c28e07b3c17a0a056830e31780e57f6d
Ilyas <ilyas.osmani@mail.utoronto.ca>
2025-11-24 10:44:12 -0500

    LoggedInOutputBoundary, LoggedInOutputData

--- src/main/java/use_case/logged_in/LoggedInOutputBoundary.java
+++ src/main/java/use_case/logged_in/LoggedInOutputBoundary.java
@@ -1,4 +1,17 @@
 package use_case.logged_in;
 
+import use_case.login.LogInOutputData;
+
 public interface LoggedInOutputBoundary {
+    void prepareSuccessView(LoggedInOutputData outputData);
+
+    /**
+     * Prepares the failure view.
+     * @param errorMessage the explanation of the failure
+     */
+    void prepareFailView(String errorMessage);
+
+    void switchToTaskView();
+    void switchToLoginView();
+
 }
--- src/main/java/use_case/logged_in/LoggedInOutputData.java
+++ src/main/java/use_case/logged_in/LoggedInOutputData.java
@@ -1,4 +1,15 @@
 package use_case.logged_in;
 
+import entity.Task;
+
+import java.util.List;
+
 public class LoggedInOutputData {
+    private final List<Task> taskList;
+    public LoggedInOutputData(List<Task> taskList) {
+        this.taskList = taskList;
+    }
+    public List<Task> getTaskList() {
+        return taskList;
+    }
 }
