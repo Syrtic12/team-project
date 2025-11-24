552e6ff435a7148d105921d8023070214752f364
DMH323 <dani.hong@mail.utoronto.ca>
2025-11-24 15:31:41 -0500

    Branch code and stuff, do not merge

--- src/main/java/interface_adapter/logged_in/LoggedInPresenter.java
+++ src/main/java/interface_adapter/logged_in/LoggedInPresenter.java
@@ -1,11 +1,55 @@
 package interface_adapter.logged_in;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.logged_in.LoggedInViewModel;
+import interface_adapter.login.LoginViewModel;
+import interface_adapter.team.TeamState;
 import interface_adapter.team.TeamViewModel;
+import use_case.logged_in.LoggedInOutputBoundary;
+import use_case.logged_in.LoggedInOutputData;
+import use_case.login.LogInOutputBoundary;
+import use_case.login.LogInOutputData;
 
-public class LoggedInPresenter {
+public class LoggedInPresenter implements LoggedInOutputBoundary {
     private final LoggedInViewModel loggedInViewModel;
+    private final LoggedInState loggedInState;
     private final ViewManagerModel viewManagerModel;
+    private final TeamViewModel teamViewModel;
+    private final LoginViewModel loginViewModel;
+    public LoggedInPresenter(LoggedInViewModel loggedInViewModel, LoggedInState loggedInState,
+                             ViewManagerModel viewManagerModel, TeamViewModel teamViewModel,
+                             LoginViewModel loginViewModel) {
+        this.loggedInViewModel = loggedInViewModel;
+        this.viewManagerModel = viewManagerModel;
+        this.teamViewModel = teamViewModel;
+        this.loggedInState = loggedInState;
+        this.loginViewModel = loginViewModel;
+    }
 
 
+    @Override
+    public void prepareSuccessView(LoggedInOutputData outputData) {
+        final TeamState teamState = teamViewModel.getState();
+        //Step 1: Use ID to get task data from DB
+        //Step 2: Get Task title and status
+        //Step 3: filter by status into map, adding the ID and Title in that order
+        //Step 4: Loop repeats for each task in LoggedInOutputData tasklist
+
+    }
+
+    @Override
+    public void prepareFailView(String errorMessage) {
+
+    }
+
+    @Override
+    public void switchToTeamView() {
+        viewManagerModel.setState(teamViewModel.getViewName());
+        viewManagerModel.firePropertyChange();
+    }
+
+    @Override
+    public void switchToLoginView() {
+        viewManagerModel.setState(loginViewModel.getViewName());
+        viewManagerModel.firePropertyChange();
+    }
 }
--- src/main/java/use_case/logged_in/LoggedInOutputBoundary.java
+++ src/main/java/use_case/logged_in/LoggedInOutputBoundary.java
@@ -11,7 +11,7 @@ public interface LoggedInOutputBoundary {
      */
     void prepareFailView(String errorMessage);
 
-    void switchToTaskView();
+    void switchToTeamView();
     void switchToLoginView();
 
 }
