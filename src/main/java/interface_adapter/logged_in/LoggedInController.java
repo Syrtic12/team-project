e78c56cd27731ab8f68dbec275563a7281217c39
DMH323 <dani.hong@mail.utoronto.ca>
2025-11-24 10:40:34 -0500

    Branch code and stuff, do not merge

--- src/main/java/interface_adapter/logged_in/LoggedInController.java
+++ src/main/java/interface_adapter/logged_in/LoggedInController.java
@@ -1,11 +1,19 @@
 package interface_adapter.logged_in;
 
+import use_case.logged_in.LoggedInInputBoundary;
+
 public class LoggedInController {
+    private final LoggedInInputBoundary loggedInInputBoundary;
+    public LoggedInController(LoggedInInputBoundary loggedInInputBoundary) {
+        this.loggedInInputBoundary = loggedInInputBoundary;
+    }
 
     public void createTeam(String teamName) {
+
     }
 
     public void logout() {
+
     }
 
     public void switchToTeamView(String teamId) {
--- src/main/java/interface_adapter/logged_in/LoggedInPresenter.java
+++ src/main/java/interface_adapter/logged_in/LoggedInPresenter.java
@@ -1,4 +1,11 @@
 package interface_adapter.logged_in;
+import interface_adapter.ViewManagerModel;
+import interface_adapter.logged_in.LoggedInViewModel;
+import interface_adapter.team.TeamViewModel;
 
 public class LoggedInPresenter {
+    private final LoggedInViewModel loggedInViewModel;
+    private final ViewManagerModel viewManagerModel;
+
+
 }
