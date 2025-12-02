e883ecda731998f542206ac2dd01124e1c4777c2
Arsen <arsenii.krokhin@mail.utoronto.ca>
2025-12-02 00:26:31 -0500

    fixed create task so it doesnt clear every other task and forces you to log out and back in

--- src/main/java/usecase/assign_task/AssignTaskInteractor.java
+++ src/main/java/usecase/assign_task/AssignTaskInteractor.java
@@ -19,25 +19,21 @@ public class AssignTaskInteractor implements AssignTaskInputBoundary {
         String taskIdx = assignTaskInputData.getTaskIdx();
         String teamMemberIdx = assignTaskInputData.getTeamMemberIdx();
         String teamLeaderIdx = assignTaskInputData.getTeamLeaderIdx();
-
         Task task = dataAccessObject.getTask(taskIdx);
         if (task == null) {
             presenter.prepareFailView("Task not found");
             return;
         }
-
         User teamMember = dataAccessObject.getUser(teamMemberIdx);
         if (teamMember == null) {
             presenter.prepareFailView("Team member not found");
             return;
         }
-
         User teamLeader = dataAccessObject.getUser(teamLeaderIdx);
         if (teamLeader == null) {
             presenter.prepareFailView("Team Leader not found");
             return;
         }
-
         Team team = dataAccessObject.getTeamByTask(taskIdx);
         if (team == null) {
             presenter.prepareFailView("This task does not belong to any team");
@@ -47,7 +43,6 @@ public class AssignTaskInteractor implements AssignTaskInputBoundary {
             presenter.prepareFailView("Only team leader can assign task members");
             return;
         }
-
         boolean alreadyAssigned = dataAccessObject.isUserAssignedToTask(taskIdx, teamMemberIdx);
         if (alreadyAssigned) {
             presenter.prepareFailView("Team member already assigned to this task");
