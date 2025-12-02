526fe09a2371fe7f1fad24bbae33e5a6b8673e2b
Arsen <arsenii.krokhin@mail.utoronto.ca>
2025-12-02 17:37:07 -0500

    hopefully finished use case checkstyle fixes for assign task

--- src/main/java/usecase/assign_task/AssignTaskOutputData.java
+++ src/main/java/usecase/assign_task/AssignTaskOutputData.java
@@ -2,6 +2,12 @@ package usecase.assign_task;
 
 import entity.Task;
 
+/**
+ * Data class representing the output of the "Assign Task" use case.
+ * Contains information about the assigned task, the team member,
+ * whether the member was already assigned, a status message, and
+ * the updated task object.
+ */
 public class AssignTaskOutputData {
     private final String taskId;
     private final String teamMemberId;
@@ -9,7 +15,8 @@ public class AssignTaskOutputData {
     private final String message;
     private final Task updatedTask;
 
-    public AssignTaskOutputData(String taskId, String teamMemberId, boolean alreadyAssigned, String message, Task updatedTask) {
+    public AssignTaskOutputData(
+            String taskId, String teamMemberId, boolean alreadyAssigned, String message, Task updatedTask) {
         this.taskId = taskId;
         this.teamMemberId = teamMemberId;
         this.alreadyAssigned = alreadyAssigned;
@@ -34,5 +41,6 @@ public class AssignTaskOutputData {
     }
 
     public Task getUpdatedTask() {
-        return updatedTask; }
+        return updatedTask;
+    }
 }
