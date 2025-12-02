c371a615b1fc7e524aee6ca30466512aedbf961e
Arsen <arsenii.krokhin@mail.utoronto.ca>
2025-12-02 18:08:14 -0500

    finished interface adapters checkstyle fixes

--- src/main/java/adapters/assign_task/AssignTaskViewModel.java
+++ src/main/java/adapters/assign_task/AssignTaskViewModel.java
@@ -2,10 +2,15 @@ package adapters.assign_task;
 
 import adapters.ViewModel;
 
+/**
+ * View model for the "Assign Task" use case.
+ * Holds the current state of the assign task operation and provides
+ * mechanisms for notifying observers of state changes.
+ */
 public class AssignTaskViewModel extends ViewModel<AssignTaskState> {
 
     public AssignTaskViewModel() {
-        super ("assign task");
+        super("assign task");
         setState(new AssignTaskState());
     }
 }
