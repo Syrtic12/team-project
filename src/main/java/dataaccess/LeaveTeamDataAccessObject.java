99d032b7fe9aa0c47d8af5240fe0c0590accfd19
Ilyas <ilyas.osmani@mail.utoronto.ca>
2025-12-01 22:38:55 -0500

    more javadocs

--- src/main/java/dataaccess/LeaveTeamDataAccessObject.java
+++ src/main/java/dataaccess/LeaveTeamDataAccessObject.java
@@ -9,19 +9,34 @@ import org.bson.types.ObjectId;
 import usecase.leave_team.LeaveTeamDataAccessInterface;
 import java.util.List;
 
+/**
+ * Data access object for the Leave Team use case.
+ * Provides methods to retrieve team and user information from MongoDB and
+ * update team membership when a user leaves a team.
+ */
 public class LeaveTeamDataAccessObject implements LeaveTeamDataAccessInterface {
     private static final String TEAMS = "teams";
     private static final String USERS = "users";
     private static final String ID = "_id";
-    private KandoMongoDatabase generalDataAccessObject;
+    private final KandoMongoDatabase generalDataAccessObject;
     private final TeamFactory teamFactory = new TeamFactory();
     private final UserFactory userFactory = new UserFactory();
 
-
+    /**
+     * Constructs the data access object with the provided database wrapper.
+     *
+     * @param dao the database interface used to interact with MongoDB collections
+     */
     public LeaveTeamDataAccessObject(KandoMongoDatabase dao) {
         generalDataAccessObject = dao;
     }
 
+    /**
+     * Retrieves a team by its ID and converts the MongoDB document into a Team entity.
+     *
+     * @param teamId the ID of the team
+     * @return the Team entity corresponding to the given ID
+     */
     @Override
     public Team getTeam(String teamId) {
         final Document teamDoc = generalDataAccessObject.getOne(TEAMS, ID, teamId);
@@ -31,6 +46,12 @@ public class LeaveTeamDataAccessObject implements LeaveTeamDataAccessInterface {
         return out;
     }
 
+    /**
+     * Retrieves a user by ID and converts the MongoDB document into a User entity.
+     *
+     * @param userId the ID of the user
+     * @return the User entity corresponding to the given ID
+     */
     @Override
     public User getUser(String userId) {
         final Document user = this.generalDataAccessObject.getOne(USERS, ID, userId);
@@ -40,6 +61,12 @@ public class LeaveTeamDataAccessObject implements LeaveTeamDataAccessInterface {
         return out;
     }
 
+    /**
+     * Retrieves the list of user IDs belonging to a specific team.
+     *
+     * @param team the team whose members should be retrieved
+     * @return a list of user IDs belonging to the team
+     */
     @Override
     public List<String> getTeamMembers(Team team) {
         final Document teamDoc = this.generalDataAccessObject.getOne(TEAMS, ID, team.getIdx());
@@ -53,13 +80,25 @@ public class LeaveTeamDataAccessObject implements LeaveTeamDataAccessInterface {
         return out;
     }
 
+    /**
+     * Retrieves the leader ID for a given team.
+     *
+     * @param team the ID of the team
+     * @return the user ID of the team leader
+     */
     @Override
     public String getTeamLeader(String team) {
-
         final Document leaderDoc = this.generalDataAccessObject.getOne(TEAMS, ID, team);
         return leaderDoc.getString("leader");
     }
 
+    /**
+     * Removes a user from a team's member list and updates the stored team document.
+     *
+     * @param teamId the ID of the team
+     * @param userId the ID of the user to remove
+     * @return true if the user was removed, false otherwise
+     */
     @Override
     public boolean removeMember(String teamId, String userId) {
         boolean result = false;
@@ -73,5 +112,4 @@ public class LeaveTeamDataAccessObject implements LeaveTeamDataAccessInterface {
         }
         return result;
     }
-
 }
--- src/main/java/usecase/leave_team/LeaveTeamDataAccessInterface.java
+++ src/main/java/usecase/leave_team/LeaveTeamDataAccessInterface.java
@@ -4,12 +4,51 @@ import entity.Team;
 import entity.User;
 import java.util.List;
 
+/**
+ * Interface defining the data access operations required for the Leave Team use case.
+ * Implementations provide persistence-layer interactions for retrieving teams,
+ * users, team membership, leaders, and updating membership when users leave teams.
+ */
 public interface LeaveTeamDataAccessInterface {
 
+    /**
+     * Retrieves a team by its ID.
+     *
+     * @param teamId the ID of the team
+     * @return the Team entity for the given ID
+     */
+    Team getTeam(String teamId);
 
-    public Team getTeam(String teamId);
-    public User getUser(String userId);
-    public List<String> getTeamMembers(Team team);
-    public String getTeamLeader(String teamid);
-    public boolean removeMember(String teamId, String userId);
+    /**
+     * Retrieves a user by their ID.
+     *
+     * @param userId the ID of the user
+     * @return the User entity for the given ID
+     */
+    User getUser(String userId);
+
+    /**
+     * Retrieves the list of user IDs belonging to the given team.
+     *
+     * @param team the team whose members are requested
+     * @return a list of user IDs
+     */
+    List<String> getTeamMembers(Team team);
+
+    /**
+     * Retrieves the leader ID of the specified team.
+     *
+     * @param teamid the ID of the team
+     * @return the user ID of the team leader
+     */
+    String getTeamLeader(String teamid);
+
+    /**
+     * Removes a user from the specified team.
+     *
+     * @param teamId the team to update
+     * @param userId the user ID to remove
+     * @return true if removal succeeded, false otherwise
+     */
+    boolean removeMember(String teamId, String userId);
 }
