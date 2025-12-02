c7510faf1d87eee77a75a58ea5c357a51479f2bf
Arsen <arsenii.krokhin@mail.utoronto.ca>
2025-12-02 17:07:24 -0500

    checkstyle fixes for some views and assigntaskdataaccessinterface

--- src/main/java/usecase/assign_task/AssignTaskDataAccessInterface.java
+++ src/main/java/usecase/assign_task/AssignTaskDataAccessInterface.java
@@ -4,10 +4,53 @@ import entity.Task;
 import entity.Team;
 import entity.User;
 
+/**
+ * Interface defining the data access operations required for the Assign Task use case.
+ * Implementations provide persistence-layer interactions for retrieving tasks, users,
+ * teams, and assigning users to tasks.
+ *
+ * @null This interface does not accept null values for method parameters.
+ */
 public interface AssignTaskDataAccessInterface {
+
+    /**
+     * Retrieves a Task entity by its unique ID.
+     *
+     * @param taskIdx the unique identifier of the task
+     * @return the Task corresponding to the given ID
+     */
     Task getTask(String taskIdx);
+
+    /**
+     * Retrieves a User entity by its unique ID.
+     *
+     * @param userIdx the unique identifier of the user
+     * @return the User corresponding to the given ID
+     */
     User getUser(String userIdx);
+
+    /**
+     * Retrieves the Team associated with a given Task.
+     *
+     * @param taskIdx the unique identifier of the task
+     * @return the Team associated with the task
+     */
     Team getTeamByTask(String taskIdx);
+
+    /**
+     * Checks whether a user is already assigned to a specific task.
+     *
+     * @param taskIdx the unique identifier of the task
+     * @param userIdx the unique identifier of the user
+     * @return true if the user is assigned to the task, false otherwise
+     */
     boolean isUserAssignedToTask(String taskIdx, String userIdx);
+
+    /**
+     * Assigns a user to a specific task.
+     *
+     * @param taskIdx the unique identifier of the task
+     * @param userIdx the unique identifier of the user to assign
+     */
     void assignUserToTask(String taskIdx, String userIdx);
 }
--- src/main/java/view/LabelTextPanel.java
+++ src/main/java/view/LabelTextPanel.java
@@ -1,8 +1,22 @@
 package view;
 
-import javax.swing.*;
+import javax.swing.JPanel;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
 
-public class LabelTextPanel extends JPanel{
+/**
+ * A simple panel that holds a label and a component side by side.
+ *
+ * @null This class does not accept null arguments for its constructor.
+ */
+public class LabelTextPanel extends JPanel {
+
+    /**
+     * Constructs a LabelTextPanel with the given label and component.
+     *
+     * @param label the JLabel to display (must not be null)
+     * @param component the JComponent to display next to the label (must not be null)
+     */
     LabelTextPanel(JLabel label, JComponent component) {
         this.add(label);
         this.add(component);
